394316fa557b216c1875d5a569833b193b05ce39
the-qmark <egormalyukow@gmail.com>
2023-10-10 21:59:11 +0300

    создал модалку с формой регистрации

---
+++ src/modals/FormModal.vue
@@ -0,0 +1,28 @@
+<template>
+  <MainModal
+    :title="title"
+    @close-modal="$emit('close-modal')"
+  >
+    <registration-form
+      :default-values="defaultValues"
+      @success="$emit('success')"
+    />
+  </MainModal>
+</template>
+
+<script>
+import MainModal from '@/components/modals/MainModal.vue';
+import RegistrationForm from '../registration-form/RegistrationForm.vue';
+
+export default {
+  name: 'FormModal',
+  components: { MainModal, RegistrationForm },
+  props: {
+    title: String,
+    defaultValues: Object,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
