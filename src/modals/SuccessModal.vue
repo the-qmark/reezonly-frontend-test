8ad3465b93455ea17a1c1c3f8cd09b3f3d720393
the-qmark <egormalyukow@gmail.com>
2023-10-10 22:01:34 +0300

    создал модалку для уведомления об успехе

---
+++ src/modals/SuccessModal.vue
@@ -0,0 +1,34 @@
+<template>
+  <MainModal
+    :title="title"
+    @close-modal="$emit('close-modal')"
+  >
+    <h4>{{ description }}</h4>
+    <custom-button
+      class="success-modal__button"
+      @click="$emit('close-modal')"
+    >
+      OK
+    </custom-button>
+  </MainModal>
+</template>
+
+<script>
+import MainModal from '@/components/modals/MainModal.vue';
+import CustomButton from '../custom-button/Button.vue';
+
+export default {
+  name: 'FormModal',
+  components: { MainModal, CustomButton },
+  props: {
+    title: String,
+    description: String,
+  },
+};
+</script>
+
+<style scoped>
+.success-modal__button {
+  width: 100%;
+}
+</style>
