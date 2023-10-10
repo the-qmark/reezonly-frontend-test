29414ff7ee7a1ebed69e7d1c6302900d36559898
the-qmark <egormalyukow@gmail.com>
2023-10-10 21:43:58 +0300

    добавил кнопки регистрации на главную страницу

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,10 +1,36 @@
 <template>
-  <div class="main-page"></div>
+  <div class="main-page">
+    <custom-button @click="openModal">
+      Зарегистрироваться с пустой формой
+    </custom-button>
+
+    <custom-button @click="openModalWithDefault">
+      Зарегистрироваться с заполненными полями
+    </custom-button>
+  </div>
 </template>
 
 <script>
+import CustomButton from '../components/custom-button/CustomButton.vue';
+
 export default {
   name: 'MainPage',
+  components: { CustomButton },
+  data() {
+    return {
+      showModal: false,
+      showModalWithDefault: false,
+      showSucess: false,
+    };
+  },
+  methods: {
+    openModal() {
+      this.showModal = true;
+    },
+    openModalWithDefault() {
+      this.showModalWithDefault = true;
+    },
+  },
 };
 </script>
 
