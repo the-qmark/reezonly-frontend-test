846f63e10aadbcb1724edca6c72289b4e4a99429
the-qmark <egormalyukow@gmail.com>
2023-10-10 21:54:36 +0300

    добавил файл с возможными ролями пользователя

---
+++ src/assets/userRoles.js
@@ -0,0 +1,16 @@
+const userRoles = [
+  {
+    value: 0,
+    name: "Роль 1",
+  },
+  {
+    value: 1,
+    name: "Роль 2",
+  },
+  {
+    value: 2,
+    name: "Роль 3",
+  },
+];
+
+export default userRoles;
