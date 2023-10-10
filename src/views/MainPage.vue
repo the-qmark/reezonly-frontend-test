6c955dd36ba301c18115d36ddf3fe7e90eb338d7
the-qmark <egormalyukow@gmail.com>
2023-10-10 21:40:15 +0300

    создал главную страницу

---
+++ src/views/MainPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="main-page"></div>
+</template>
+
+<script>
+export default {
+  name: 'MainPage',
+};
+</script>
+
+<style scoped>
+.main-page {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 30px;
+}
+</style>
